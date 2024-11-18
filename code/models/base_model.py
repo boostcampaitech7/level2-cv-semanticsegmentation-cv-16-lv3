01ada79d302aa506f29429aeaefec72c96f130cf
eunseop <ddf159@naver.com>
2024-11-18 02:24:52 +0000

    [feat]#28 - 채널 1개를 conv로 3채로 늘리고 학습하기 위한 모델 추가

--- code/models/base_model.py
+++ code/models/base_model.py
@@ -24,3 +24,16 @@ class DeepLabV3PlusModel(nn.Module):
 
     def forward(self, x: torch.Tensor):
         return self.model(x)
+    
+class DeepLabV3PlusModel_channel0(nn.Module):
+    """
+    Base Model DeepLabV3Plus
+    """
+    def __init__(self, **kwargs):
+        super(DeepLabV3PlusModel_channel0, self).__init__()
+        self.additional_conv = nn.Conv2d(1, 3, kernel_size=3, padding=1)
+        self.model = smp.DeepLabV3Plus(**kwargs)
+        
+    def forward(self, x: torch.Tensor):
+        x = self.additional_conv(x)        
+        return self.model(x)
