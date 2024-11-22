4d2544c19fcb8dbe44ea2b2300e964c5b8482306
eunseop <ddf159@naver.com>
2024-11-22 14:36:34 +0000

    [Feat] - base_model.py 수정

--- code/models/base_model.py
+++ code/models/base_model.py
@@ -11,13 +11,18 @@ class UnetModel(nn.Module):
         super(UnetModel, self).__init__()
         self.model = smp.Unet(**model_parameters)
         
-        if model_parameters.get("lora_use", False):  
-            lora_config = model_parameters.get("lora_config")
-            lora_config = peft.LoraConfig(**lora_config)
-            self.model = peft.get_peft_model(self.model, lora_config)
             
     def forward(self, x: torch.Tensor):
         return self.model(x)
+    
+    
+    def load_pretrained_weights(self, checkpoint_path: str):
+        self.model = torch.load(checkpoint_path)
+        
+        
+    def apply_lora(self, lora_config: dict):
+        lora_config = peft.LoraConfig(**lora_config)
+        self.model = peft.get_peft_model(self.model, lora_config)
     
 class DeepLabV3PlusModel(nn.Module):
     """
@@ -30,19 +35,13 @@ class DeepLabV3PlusModel(nn.Module):
     
     def forward(self, x: torch.Tensor):
         return self.model(x)
-
-
+ 
+ 
     def load_pretrained_weights(self, checkpoint_path: str):
-        """
-        Load pretrained weights from checkpoint
-        Args:
-            checkpoint_path (str): Path to the checkpoint file
-        """
         self.model = torch.load(checkpoint_path)
         
-
+        
     def apply_lora(self, lora_config: dict):
-
         lora_config = peft.LoraConfig(**lora_config)
         self.model = peft.get_peft_model(self.model, lora_config)
         
@@ -56,16 +55,20 @@ class DeepLabV3PlusModel_channel0(nn.Module):
         super(DeepLabV3PlusModel_channel0, self).__init__()
         self.additional_conv = nn.Conv2d(1, 3, kernel_size=3, padding=1)
         self.model = smp.DeepLabV3Plus(**model_parameters)
-
-        if model_parameters.get("lora_use", False):  
-            lora_config = model_parameters.get("lora_config")
-            lora_config = peft.LoraConfig(**lora_config)
-            self.model = peft.get_peft_model(self.model, lora_config)
                 
     def forward(self, x: torch.Tensor):
         x = self.additional_conv(x)        
         return self.model(x)
         
+        
+    def load_pretrained_weights(self, checkpoint_path: str):
+        self.model = torch.load(checkpoint_path)
+        
+        
+    def apply_lora(self, lora_config: dict):
+        lora_config = peft.LoraConfig(**lora_config)
+        self.model = peft.get_peft_model(self.model, lora_config)
+
 
 class UnetPlusPlus(nn.Module):
     """
@@ -75,12 +78,16 @@ class UnetPlusPlus(nn.Module):
         super(UnetPlusPlus, self).__init__()
         self.model = smp.UnetPlusPlus(**model_parameters)
         
-        if model_parameters.get("lora_use", False):  
-            lora_config = model_parameters.get("lora_config")
-            lora_config = peft.LoraConfig(**lora_config)
-            self.model = peft.get_peft_model(self.model, lora_config)
-
+        
     def forward(self, x: torch.Tensor):
-
         return self.model(x)
+    
+
+    def load_pretrained_weights(self, checkpoint_path: str):
+        self.model = torch.load(checkpoint_path)
+        
+        
+    def apply_lora(self, lora_config: dict):
+        lora_config = peft.LoraConfig(**lora_config)
+        self.model = peft.get_peft_model(self.model, lora_config)
     
